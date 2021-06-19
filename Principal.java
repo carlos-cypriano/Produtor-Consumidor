ab5a57e092768afb09dc1689708f5ea69a1aed9a
carlos-cypriano <81194143+carlos-cypriano@users.noreply.github.com>
2021-06-19 20:57:42 -0300

    Construindo classe Principal
    De início é definido um número de permissões disponíveis pelo Semaforo, podendo esse número ser alterado. Em seguida são iniciados o mutex, buffer_cheio, buffer_vazio e uma instância da classe Buffer.

--- Principal.java
+++ Principal.java
@@ -1,3 +1,15 @@
+import java.util.concurrent.Semaphore; 
+
 public class Principal {
     
+    public static void main(String[] args) {
+        /*Permite que apenas um consumidor consuma, enquanto não termina o consumo, o produtor não pode liberar o item
+        numero de permissoes pode ser alterado*/
+        int numeroPermissoes = 1;
+        Semaforo mutex = new Semaforo(1);   //parecido com o semaforo, porém não há notificação e só permite uma thread por vez 
+        Semaforo buffer_cheio = new Semaforo(0);    //inicia o buffer do zero
+        Semaforo buffer_vazio = new Semaforo(numeroPermissoes);
+
+        Buffer buffe = new Buffer(new int[5], mutex, buffer_cheio, buffer_vazio);
+    }
 }
